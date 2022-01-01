9a370a75b6a646151ac2517e3ce948a75bde2453
Léo Amsallem <leo.amsallem@etu.univ-grenoble-alpes.fr>
2022-01-01 23:40:07 +0100

    add semantic

--- client/src/index.js
+++ client/src/index.js
@@ -1,14 +1,21 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React from "react";
+
+import ReactDOM from "react-dom";
+import "./index.css";
+import App from "./components/App/App.jsx";
+import reportWebVitals from "./reportWebVitals";
+
+const styleLink = document.createElement("link");
+styleLink.rel = "stylesheet";
+styleLink.href =
+  "https://cdn.jsdelivr.net/npm/semantic-ui@2/dist/semantic.min.css";
+document.head.appendChild(styleLink);
 
 ReactDOM.render(
   <React.StrictMode>
     <App />
   </React.StrictMode>,
-  document.getElementById('root')
+  document.getElementById("root")
 );
 
 // If you want to start measuring performance in your app, pass a function

3be8146a2fbb5896ef98d7e4ce91acd4c5f069d1
Léo Amsallem <leo.amsallem@etu.univ-grenoble-alpes.fr>
2022-01-02 15:05:12 +0100

    chore: add cors package

--- server.js
+++ server.js
@@ -2,14 +2,16 @@
 const express = require("express");
 const app = express();
 const port = 3001;
+var cors = require("cors");
 
-// create a GET route
-app.get("/express_backend", (req, res) => {
-  //Line 9
-  res.send({ express: "YOUR EXPRESS BACKEND IS CONNECTED TO REACT" }); //Line 10
-}); //Line 11
+app.use(cors());
+
+const musiciansRouter = require("./routes/musicians");
+
+//routes
+app.use(musiciansRouter);
 
 //express listen on port 3000
 app.listen(port, () => {
-  console.log("listening on port 3001");
+  console.log(`istening on port ${port})`);
 });
